90b35217b31845a28fb4b7a73e26d5c3c2ffde88
Shishak <semenshishak@gmail.com>
2020-01-31 16:05:59 +0200

    Initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,20 @@
+siteName: "Gridsome",
+  module.exports = {
+  siteDescription: "A WordPress starter for Gridsome",
+
+  templates: {
+    WordPressCategory: "/category/:slug", // adds route for "category" post type (Optional)
+    WordPressPost: "/:year/:month/:day/:slug", //adds route for "post" post type (Optional)
+    WordPressPostTag: "/tag/:slug" // adds route for "post_tag" post type (Optional)
+  },
+
+  plugins: [
+    {
+      use: "@gridsome/source-wordpress",
+      options: {
+        baseUrl: "https://wp-news-api.herokuapp.com/", // required
+        typeName: "WordPress" // GraphQL schema name (Optional)
+      }
+    }
+  ]
+};
---
+++ src/components/Post.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2 v-html="post.title"/>
+    <g-link :to="post.path">Read More, here</g-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
